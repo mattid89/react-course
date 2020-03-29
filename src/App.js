ecdd27e2ede9a52dbd51b99459db5a049c964088
Matias Doto <matydoto@gmail.com>
2020-03-29 02:38:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I'm a React App!</h1>
+      <p>Probando un texto</p>
+      <Person name="Max" age="28" />
+      <Person name="Manu" age="26">
+        My Hobbies: Racing
+      </Person>
+      <Person name="Stephanie" age="29" />
     </div>
   );
+  // React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Ahora si anda...'))
 }
 
 export default App;
