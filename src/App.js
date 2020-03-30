f8236cf95b5e272e867e69a3781e880502df9bd0
Matias Doto <matydoto@gmail.com>
2020-03-30 00:55:17 -0300

    jugando con el map para enlistar un array

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App!</h1>
-      <p>Probando un texto</p>
-      <Person name="Max" age="28" />
-      <Person name="Manu" age="26">
-        My Hobbies: Racing
-      </Person>
-      <Person name="Stephanie" age="29" />
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 26 },
+      { name: 'Stephanie', age: 29 }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App!</h1>
+        <p>Probando un texto</p>
+        <button>Cambiar Nombre</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+
+        {/* <div className="card-wrapper">
+          {this.state.persons.map( person => {
+            return (
+              <div className="card">
+                <div className="container">
+                  <h4><b>{person.name}</b></h4> 
+                  <p>Profesión: Architect & Engineer</p> 
+                  <p>Edad: {person.age}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div> */}
+      </div>
+    );
+  };
   // React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Ahora si anda...'))
 }
 
